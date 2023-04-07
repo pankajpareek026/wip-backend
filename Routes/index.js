46ab960d87fda68b9615818e544cfca7282208c8
Pankaj Pareek <71495471+pankajpareek026@users.noreply.github.com>
2023-04-07 04:31:18 +0000

    #

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import authController from "../controllers/userAuthController.js";
 import { rateLimitor, auth } from '../middlewares/index.js'
-import refferalService from '../services/refferalService'
+import refferalService from '../services/refferalService.js'
 const { registerController, otpController, plansController, loginController, depositController, depositAddressController, modifyDepositAddressController, refferController, withdrawController, transactionsController, userController, adminControllers, dashBoardController } = require('../controllers/index.js')
 const express = require('express');
 const router = express.Router();
